08f3ba14d3027b4453f472093b1b3731f0d7ecbe
milcamboo <milcamboo@gmail.com>
2022-08-24 23:26:32 +0900

    feat: wrote subscriber model logic

--- src/db/models/subscriber-model.js
+++ src/db/models/subscriber-model.js
@@ -1,17 +1,19 @@
-// subscriber model logic
-import { SubscriberSchema } from "../schemas/index";
+import { Subscriber } from "..";
 export class SubscriberModel {
 // 1. 구독 신청
-    async subscribe(){
-
+    async subscribe(subscribeInfo){
+        const result = Subscriber.create(subscribeInfo);
+        return result;
     };
 // 2. 구독 취소
-    async cancelSubscription() {
-
+    async cancelSubscription(email) {
+        const result = Subscriber.findOneAndDelete({ email });
+        return result;
     }
 // 3. 구독 조회
     async findSubscriber(){
-
+        const result = Subscriber.findOne({ email });
+        return result;
     }
 }
 
