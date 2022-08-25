d5789df7b55b29814224a675e0d30af8b84641ae
milcamboo <milcamboo@gmail.com>
2022-08-26 02:11:10 +0900

    added access-control-allow-origin config

--- src/app.js
+++ src/app.js
@@ -11,12 +11,24 @@ job.job();
 // jobService.insertJobs();
 
 const corsOptions = {
-    origin : '*'
+    origin : 
+	[
+    'http://localhost:3000',
+    '*',
+  	],
+	credentials:true,
+	optionSuccessStatus:200
 }
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 app.use('/api', apiRouter);
 
